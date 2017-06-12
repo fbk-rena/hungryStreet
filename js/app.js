7bbaa94a8958554be9f86e7822029f0f08429229
fbk-rena <fbk.rena@gmail.com>
2017-06-11 23:47:36 -0500

    Estructuracion de carpetas y HTML, enlace Google Map API

---
+++ js/app.js
@@ -0,0 +1,26 @@
+ var obtenerUbicacion = function (e) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrarPosicion);
+    } else {
+        alert("Actualice su navegador");
+    }
+};
+
+var mostrarPosicion = function (posicion) {
+    var coordenadas = {
+        lat: posicion.coords.latitude,
+        lng: posicion.coords.longitude
+    };
+    mostrarMapa(coordenadas);
+};
+
+var mostrarMapa = function (coordenadas) {
+    var mapa = new google.maps.Map($('#map'), {
+      zoom: 9,
+      center: coordenadas
+    });
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: mapa
+    });
+}
